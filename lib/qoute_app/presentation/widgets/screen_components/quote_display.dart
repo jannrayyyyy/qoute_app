64646dbaeac3bf29a342900d804b91f1c658a0d0
Jannray Mostajo <jannmostajo02@gmail.com>
2022-07-12 21:37:40 -0700

    day 2

---
+++ lib/qoute_app/presentation/widgets/screen_components/quote_display.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:qoute/qoute_app/domain/entity/qoute_entity.dart';
+import 'package:qoute/qoute_app/presentation/widgets/primary_widgets/custom_text_widget.dart';
+
+import '../../../domain/entity/favorite_entity.dart';
+import '../../cubit/favorite_cubit/add_fav_cubit.dart';
+
+class QuoteDisplayWidget extends StatefulWidget {
+  final QouteEntity qoute;
+  const QuoteDisplayWidget({Key? key, required this.qoute}) : super(key: key);
+
+  @override
+  State<QuoteDisplayWidget> createState() => _QuoteDisplayWidgetState();
+}
+
+class _QuoteDisplayWidgetState extends State<QuoteDisplayWidget> {
+  bool isFav = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(3),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          QuickSandTextWidget(
+            text: widget.qoute.text,
+            fontsize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+          const SizedBox(height: 20),
+          QuickSandTextWidget(
+            text: widget.qoute.author,
+            fontsize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+          const SizedBox(height: 200),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const InkWell(child: Icon(Icons.share, size: 25)),
+              const SizedBox(width: 20),
+              InkWell(
+                onTap: () {
+                  final fav = FavoriteEntity(
+                      text: widget.qoute.text, author: widget.qoute.author);
+                  context.read<FavCubit>().addFavorite(fav);
+                  setState(() {
+                    isFav = !isFav;
+                  });
+                },
+                child: isFav
+                    ? const Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                        size: 25,
+                      )
+                    : const Icon(Icons.favorite_border),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+  //  BlocProvider<FavCubit>(
+  //                   create: (context) => sl<FavCubit>()..addFavorite(fav),
+  //                 );
+
+  // context.read<FavCubit>().addFavorite(favorite);
